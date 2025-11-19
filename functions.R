4fceb9054e22b3ef44877fdc1c2cddf689742479
spertus <jakespertus@gmail.com>
2025-11-19 07:50:04 -0500

    original files copied from soil-carbon-statistics repo

---
+++ functions.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(data.table)
+library(gstat)
+#permuter can be downloaded from Github by following README: https://github.com/statlab/permuter
+library(permuter)
+#library(kolmim)
+
+
+
+
